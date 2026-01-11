8a37295ea000cd1d828a729493b543bb5bb9fab0
quanzz123 <nguyenquandc37@gmail.com>
2026-01-11 12:25:10 +0700

    create LessonController

---
+++ src/main/java/com/example/eduSystems/controllers/admin/LessonController.java
@@ -0,0 +1,10 @@
+package com.example.eduSystems.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/lessons")
+public class LessonController {
+
+}
